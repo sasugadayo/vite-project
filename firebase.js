8e75b962e8aea2362832d0184f980d7429f04983
sasugadayo <sasugaff@icloud.com>
2022-03-31 21:51:38 +0900

    firebase input form

--- firebase.js
+++ firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBXL_B0vwGgjlyNgNSxfp81aWHBIGwPaSk",
   authDomain: "crud-56522.firebaseapp.com",
@@ -13,26 +11,9 @@ const firebaseConfig = {
   appId: "1:157071779506:web:a8d24c5aa69013d5748bab"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+export const db = getFirestore();
+export const storage = getStorage();
 
-// import { initializeApp } from 'firebase/app';
-// import { getFirestore } from 'firebase/firestore';
-// import { getStorage } from 'firebase/storage';
-
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyBl6ETJSqdRDdKJ4u7l3uJXtS2KR6E0ic',
-//   authDomain: 'my-project-6fe6.firebaseapp.com',
-//   projectId: 'my-project-6fe6',
-//   storageBucket: 'my-project-6fe6.appspot.com',
-//   messagingSenderId: '95911302504',
-//   appId: '1:959113072504:web:580ab982e4a63a164428d',
-// };
-
-// const app = initializeApp(firebaseConfig);
-
-// export const db = getFirestore();
-// export const storage = getStorage();
-
-// export default app;
+export default app;
--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,48 @@
+
 import { useState, useEffect } from 'react';
 import { db } from '../firebase';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, onSnapshot } from 'firebase/firestore';
 
 function App() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     const usersCollectionRef = collection(db, 'users');
-    getDocs(usersCollectionRef).then((querySnapshot) => {
+    const unsub = onSnapshot(usersCollectionRef, (querySnapshot) => {
       setUsers(
         querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
       );
     });
+    return unsub;
   }, []);
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { name, email } = event.target.elements;
+    console.log(name.value, email.value);
+  };
+
   return (
-    <div>
-      {users.map((user) => (
-        <div key={user.id}>{user.name}</div>
-      ))}
+    <div style={{ margin: '50px' }}>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>名前</label>
+          <input name="name" type="text" placeholder="名前" />
+        </div>
+        <div>
+          <label>メールアドレス</label>
+          <input name="email" type="email" placeholder="メールアドレス" />
+        </div>
+        <div>
+          <button>登録</button>
+        </div>
+      </form>
+      <h1>ユーザ一覧</h1>
+      <div>
+        {users.map((user) => (
+          <div key={user.id}>{user.name}</div>
+        ))}
+      </div>
     </div>
   );
 }
